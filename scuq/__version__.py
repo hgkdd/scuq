9bf9cac88a0672d675f636a8bcd76364053962be
Hans Georg Krauthäuser <hgk@ieee.org>
2009-04-30 12:25:21 +0200

    directory layout optimized for distutils

---
+++ scuq/__version__.py
@@ -0,0 +1,2 @@
+# This file is auto-generated.
+version = 'bee519b0c2e4+20090430'
--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
+import os
 from distutils.core import setup
+import version
+
+ver=''
+version.remember_version(ver)
+
 setup(name='scuq',
-      version='1.0',
-      package_dir={'': 'scuq'},
-      packages=[''],
+      version=version.get_version(),
+      author="Thomas Reidemeister, Hans Georg Krauthaeuser",
+      author_email="hgk@ieee.org",
+      description='scalar and complex uncertain pysical quantities',
+      license='All rights reserved',
+      url='http://tu-dresden.de/et/tet',
+	  packages=['scuq'],
+      data_files=[('scuq', ['scuq/LICENSE'])],
       )
